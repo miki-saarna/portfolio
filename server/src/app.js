05843110154f04bff6343c64e9b258b01d0ede34
Mikito Saarna <mikitosaarna@gmail.com>
2022-02-02 21:15:49 -0600

    Updated mongoDB projects schema to include _id

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const projectsRouter = require('./projects/projects.router');
+
+app.use(cors({
+    origin: 'http://localhost:3000'
+}))
 
 // app.use(express.json());
 
-app.use('/', projectsRouter);
+app.use('/projects', projectsRouter);
 
 module.exports = app;
--- server/src/mongoDB/accessProjects.js
+++ server/src/mongoDB/accessProjects.js
@@ -6,6 +6,9 @@ db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('Connected to database!'));
 
 const ProjectSchema = new mongoose.Schema({
+    _id: {
+        type: String
+    },
     github_link: {
         type: String
     },
@@ -19,7 +22,7 @@ const ProjectSchema = new mongoose.Schema({
         type: String
     },
     languages: {
-        type: String
+        type: Array
     }
 })
 
