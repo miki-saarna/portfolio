86261257851e16f22d61fd652597c042fef022bf
Mikito Saarna <mikitosaarna@gmail.com>
2022-02-06 21:57:03 -0600

    Imported nodemailer package into backend

--- server/src/app.js
+++ server/src/app.js
@@ -2,13 +2,15 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const projectsRouter = require('./projects/projects.router');
+const formRouter = require('./form/form.router');
 
 app.use(cors({
     origin: 'http://localhost:3000'
 }))
 
-// app.use(express.json());
+app.use(express.json());
 
 app.use('/projects', projectsRouter);
+app.use('/form', formRouter);
 
 module.exports = app;
