983b3566bc13b8d01130cc2846b3b03e13d98d34
Mikito Saarna <mikitosaarna@gmail.com>
2022-02-11 23:28:15 -0600

    Attempted event listener on project cards

--- client/src/portfolio/ProjectCards.jsx
+++ client/src/portfolio/ProjectCards.jsx
@@ -35,7 +35,6 @@ export default function ProjectCards ({projectsList, setCardSelected}) {
             parallaxPosition.forEach((element) => {
                 element.style.position = `static`;
             })
-            // parallaxPosition.style.position = `static`;
         }
         
         return (
