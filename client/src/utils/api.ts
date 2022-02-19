67e131e462bcc5b8a24220cddba68fcd492cbe60
Mikito Saarna <mikitosaarna@gmail.com>
2022-02-18 18:50:29 -0600

    Added type annotations to HomePage

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -1,8 +1,9 @@
+import { Project } from './types';
 const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000';
 const headers = new Headers();
 headers.append('Content-type', 'application/json');
 
-export const retrieveProjects = async (signal) => {
+export const retrieveProjects = async (signal): Promise<Project[]> => {
     const url:any = new URL(`${API_BASE_URL}/projects`);
     const config = {
         method: 'GET',
--- client/src/utils/types.ts
+++ client/src/utils/types.ts
@@ -2,3 +2,19 @@ export interface MenuDataEntry {
     title: string,
     path: string
 }
+
+export interface Project {
+    description: string,
+    github_link: string,
+    image: string,
+    languages: Array<string>,
+    name: string,
+    _id: string
+}
+
+export interface TextAnimationDataEntry {
+    class_name: string,
+    revealVariable: Array<number>
+    // above and below are 2 different ways to indicate an array
+    hideVariable?: number[]
+}
